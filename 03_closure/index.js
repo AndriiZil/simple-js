def51086d23615e4549fb98a4377734da691b4e3
Andrii Zilnyk <andrii.zilnyk@gmail.com>
2020-06-24 20:01:50 +0300

    added closure

---
+++ 03_closure/index.js
@@ -0,0 +1,65 @@
+function createCalcFunction(n) {
+    return function () {
+        console.log(1000 * n);
+    }
+}
+
+const calc = createCalcFunction(5);
+
+console.log(calc()); // => 5000
+
+function createIncrementor(n) {
+    return function (num) {
+        return n + num; // => 1
+    }
+}
+
+const addOne = createIncrementor(1);
+const addTen = createIncrementor(10);
+
+console.log(addOne(10)); // => 11
+console.log(addTen(15)); // => 25
+
+function urlUrlGenerator(domain) {
+    return function (url) {
+        return `https://${url}.${domain}`;
+    }
+}
+
+const comUrl = urlUrlGenerator('com');
+const ruUrl = urlUrlGenerator('ru');
+
+console.log(comUrl('google')); // => https://google.com
+console.log(comUrl('netflix')); // => https://netflix.com
+
+console.log(ruUrl('yandex')); // => https://yandex.ru
+console.log(ruUrl('vk')); // => https://vk.ru
+
+// bind function
+function bind(ctx, fn) {
+    return function (...args) {
+        fn.apply(ctx, args);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
